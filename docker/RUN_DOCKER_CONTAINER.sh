c8d180a7e0f4c271ffdaad0056f9f10a6da8345b
TMats <t.matsu.changmin@gmail.com>
2023-05-26 19:02:03 +0900

    Enable mount datasets from other places

--- docker/RUN_DOCKER_CONTAINER.sh
+++ docker/RUN_DOCKER_CONTAINER.sh
@@ -9,6 +9,11 @@ fi
 
 export WANDB_API_KEY=`cat wandb_key.txt`
 
+if [ ! -z ${DATASET_DIR} ]; then
+  echo "DATASET_DIR is set to ${DATASET_DIR}"
+else
+  export DATASET_DIR=${PWD}/../data
+  echo "DATASET_DIR is not set, using default value instead: ${DATASET_DIR}"
+fi
+
 docker-compose up ${TAG_NAME} &
-
-
