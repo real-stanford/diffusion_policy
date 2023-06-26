0dbd09b53dfd8d4e0228a17bc9e6e0c86056398f
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-06-26 15:08:45 -0700

    fixed upsampling so that h_local[1] is actually used

--- diffusion_policy/model/diffusion/conditional_unet1d.py
+++ diffusion_policy/model/diffusion/conditional_unet1d.py
@@ -226,7 +226,7 @@ class ConditionalUnet1D(nn.Module):
         for idx, (resnet, resnet2, upsample) in enumerate(self.up_modules):
             x = torch.cat((x, h.pop()), dim=1)
             x = resnet(x, global_feature)
-            if idx == len(self.up_modules) and len(h_local) > 0:
+            if idx == (len(self.up_modules)-1) and len(h_local) > 0:
                 x = x + h_local[1]
             x = resnet2(x, global_feature)
             x = upsample(x)
