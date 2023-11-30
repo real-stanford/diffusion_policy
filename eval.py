4e0267cc69ac04370829c283a7885cd3e714cd2a
-T.K.- <t_k_233@outlook.com>
2023-11-29 22:38:02 -0800

    ADD: add type hint, fix import order, refactor code style

--- eval.py
+++ eval.py
@@ -5,43 +5,46 @@ python eval.py --checkpoint data/image/pusht/diffusion_policy_cnn/train_0/checkp
 
 import sys
 # use line-buffering for both stdout and stderr
-sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
-sys.stderr = open(sys.stderr.fileno(), mode='w', buffering=1)
-
-from isaacgym.torch_utils import *
-
+sys.stdout = open(sys.stdout.fileno(), mode="w", buffering=1)
+sys.stderr = open(sys.stderr.fileno(), mode="w", buffering=1)
 import os
 import pathlib
 import click
+import json
+
 import hydra
-import torch
+from omegaconf import OmegaConf
 import dill
 import wandb
-import json
-from omegaconf import OmegaConf
+from isaacgym.torch_utils import *
+import torch
 
+from diffusion_policy.env_runner.diffsionrobot_lowdim_isaac_runner import IsaacHumanoidRunner
 from diffusion_policy.workspace.base_workspace import BaseWorkspace
 
 @click.command()
-@click.option('-c', '--checkpoint', required=True)
-@click.option('-o', '--output_dir', required=True)
-@click.option('-d', '--device', default='cuda:0')
+@click.option("-c", "--checkpoint", required=True)
+@click.option("-o", "--output_dir", required=True)
+@click.option("-d", "--device", default="cuda:0")
 def main(checkpoint, output_dir, device):
     if os.path.exists(output_dir):
         click.confirm(f"Output path {output_dir} already exists! Overwrite?", abort=True)
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     
     # load checkpoint
-    payload = torch.load(open(checkpoint, 'rb'), pickle_module=dill)
-    cfg = payload['cfg']
+    payload = torch.load(open(checkpoint, "rb"), pickle_module=dill)
+    cfg = payload["cfg"]
+    
+    cfg["task"]["env_runner"]["_target_"] = "diffusion_policy.env_runner.diffsionrobot_lowdim_isaac_runner.IsaacHumanoidRunner"
+    
     action_steps = 4
-    cfg['task']['env_runner']['_target_'] = 'diffusion_policy.env_runner.diffsionrobot_lowdim_isaac_runner.IsaacHumanoidRunner'
-    cfg['n_action_steps'] = action_steps
-    cfg['task']['env_runner']['n_action_steps'] = action_steps
-    cfg['policy']['n_action_steps'] = action_steps
+    cfg["n_action_steps"] = action_steps
+    cfg["task"]["env_runner"]["n_action_steps"] = action_steps
+    cfg["policy"]["n_action_steps"] = action_steps
+    
     OmegaConf.set_struct(cfg, False)
 
-    cfg.task.env_runner['device'] = device
+    cfg.task.env_runner["device"] = device
     
     cls = hydra.utils.get_class(cfg._target_)
     
@@ -64,6 +67,7 @@ def main(checkpoint, output_dir, device):
     policy.eval()
     
     # run eval
+    env_runner: IsaacHumanoidRunner
     env_runner = hydra.utils.instantiate(
         cfg.task.env_runner,
         output_dir=output_dir)
@@ -76,8 +80,8 @@ def main(checkpoint, output_dir, device):
             json_log[key] = value._path
         else:
             json_log[key] = value
-    out_path = os.path.join(output_dir, 'eval_log.json')
-    json.dump(json_log, open(out_path, 'w'), indent=2, sort_keys=True)
+    out_path = os.path.join(output_dir, "eval_log.json")
+    json.dump(json_log, open(out_path, "w"), indent=2, sort_keys=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
